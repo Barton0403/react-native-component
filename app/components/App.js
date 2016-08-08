35f4645c48d4ba10bf09172940074d4ec16a735d
li chenghao <lichenghao0403@gmail.com>
2016-08-09 00:44:56 +0800

    项目创建
    初始化React-Native项目，建立目录结构

---
+++ app/components/App.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import BTRow from './BTRow'
+
+class App extends Component {
+  render() {
+    return (
+      <BTRow />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default App;
---
+++ app/components/BTRow.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+class BTRow extends Component {
+  render() {
+    return (
+      <View>
+      	<Text>test</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default BTRow;
